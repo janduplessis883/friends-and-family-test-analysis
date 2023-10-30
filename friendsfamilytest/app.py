9b39e4bbc5f704472b14a47cf798b5f11df2cd13
Jan du Plessis <drjanduplessis@icloud.com>
2023-10-30 03:17:50 +0000

    update streamlit app

--- friendsfamilytest/app.py
+++ friendsfamilytest/app.py
@@ -25,13 +25,12 @@ monthly_avg_df = monthly_avg.reset_index()
 monthly_avg_df.columns = ['Month', 'Average Rating']
 
 st.sidebar.title('Menu') 
-page = st.sidebar.selectbox('Choose an option', ['Monthly Rating', 'Monthly Count', 'Sentiment Analysis Histplot', 'Rating & Sentiment Correlation', 'Text Classification', 'Word Cloud', 'Show Raw Data', 'About'])
-st.sidebar.slider('View data for last n months?', min_value=1, max_value=12, value=1)
+page = st.sidebar.selectbox('Choose an option', ['Monthly Rating', 'Monthly Count', 'Sentiment Analysis Histogram', 'Rating & Sentiment Correlation', 'Text Classification', 'Word Cloud', 'Show Raw Data', 'About'])
+
 
 # Display content based on the selected page
 if page == 'Monthly Rating':
-    st.title('Friends & Family Test Monthly Rating')
-    st.write('The monthly rating by rating score.')
+    st.image('https://github.com/janduplessis883/friends-and-family-test-analysis/blob/master/images/fftest2.png?raw=true', use_column_width=True)
     
     # Plot monthly averages
     st.subheader('Monthly Average Rating')
@@ -51,11 +50,11 @@ if page == 'Monthly Rating':
 
     # Display the plot in Streamlit
     st.pyplot(fig)
+    st.write('Rating Scale 1 - 5')
     
 elif page == 'Monthly Count':
-    st.title('Monthly Count')
-    st.write('Showing the number of FF Test responses received per month.')
-    
+    st.image('https://github.com/janduplessis883/friends-and-family-test-analysis/blob/master/images/fftest2.png?raw=true', use_column_width=True)
+    st.subheader('Monthly Count')
     # Resample and count the entries per day
     monthly_count = data_time.resample('M').size()
     
@@ -64,7 +63,7 @@ elif page == 'Monthly Count':
     
     # Plotting
     fig, ax = plt.subplots(figsize=(12, 6))
-    sns.barplot(data=monthly_count, x='time', y='entry_count', color='#a54b49')
+    sns.barplot(data=monthly_count, x='time', y='entry_count', color='#e3813c')
     
     # Customizing x-axis labels
     n = len(monthly_count['time'])
@@ -75,10 +74,9 @@ elif page == 'Monthly Count':
     # Show the plot in Streamlit
     st.pyplot(fig)
     
-elif page == 'Sentiment Analysis Histplot':
-    st.title('Sentiment Analysis Histplot')
-    st.write('You are viewing the content of Page 2.')
-    
+elif page == 'Sentiment Analysis Histogram':
+    st.image('https://github.com/janduplessis883/friends-and-family-test-analysis/blob/master/images/fftest2.png?raw=true', use_column_width=True)
+    st.subheader('Sentiment Analysis Histogram')
     # Filter the data for 'Negative' and 'Positive' values in the 'score3' column
     data_positive = data[data['label3'] == 'positive']['score3']
     data_negative = data[data['label3'] == 'negative']['score3']
@@ -87,7 +85,7 @@ elif page == 'Sentiment Analysis Histplot':
     fig, ax = plt.subplots(figsize=(10, 6))
 
     # Plot the histograms
-    sns.histplot(data_positive, color='#69a443', bins=20, kde=True, label='Positive', ax=ax)
+    sns.histplot(data_positive, color='#e4c046', bins=20, kde=True, label='Positive', ax=ax)
     sns.histplot(data_negative, color='#a54b49', bins=20, kde=True, label='Negative', ax=ax)
 
     # Add title and labels
@@ -100,34 +98,35 @@ elif page == 'Sentiment Analysis Histplot':
     st.pyplot(fig)
     
 elif page == 'Rating & Sentiment Correlation':
-    st.title('Rating & Sentiment Correlation')
- 
+    st.image('https://github.com/janduplessis883/friends-and-family-test-analysis/blob/master/images/fftest2.png?raw=true', use_column_width=True)
+    st.subheader('Sentiment Anaylsis & Rating Correlation')
 
-    st.subheader('Correlation between score3 and rating_score')
     correlation = data['score3'].corr(data['rating_score'])
     st.write(f'Correlation: {correlation}')
     
     # Scatter Plot
-    fig1, ax1 = plt.subplots(figsize=(10,4))
-    ax1.scatter(data['score3'], data['rating_score'], color='#a54b49', alpha=0.7, marker="x")
+    fig1, ax1 = plt.subplots(figsize=(10,6))
+    ax1.scatter(data['score3'], data['rating_score'], color='#749857', alpha=0.7, marker="x")
     ax1.set_title('Scatter Plot of Sentiment Analysis Score vs Rating Score')
     ax1.set_xlabel('Sentiment Analysis Score')
     ax1.set_ylabel('Rating Score')
     st.pyplot(fig1)
     
 elif page == 'Text Classification':
-    st.title('Text Classification')
-    st.write('You are viewing the content of Page 2.')
+    st.image('https://github.com/janduplessis883/friends-and-family-test-analysis/blob/master/images/fftest2.png?raw=true', use_column_width=True)
+    st.subheader('Text Classification')
     
    # Prepare the data
     label_counts = data['label1'].value_counts().reset_index()
     label_counts.columns = ['label', 'count']
+    label_counts2 = data['label2'].value_counts().reset_index()
+    label_counts2.columns = ['label2', 'count']
 
     # Sort the dataframe by count
     label_counts = label_counts.sort_values(by='count', ascending=False)
-
+    label_counts2 = label_counts2.sort_values(by='count', ascending=False)
     # Display DataFrame with index set to 3
-    st.write('Text Classification Frequency')
+    st.write('Text Classification Frequency: Label 1')
 
     # Plot using seaborn
     fig3, ax3 = plt.subplots(figsize=(10, 6))
@@ -135,11 +134,23 @@ elif page == 'Text Classification':
     
     # Display the plot in Streamlit
     st.pyplot(fig3)
+    st.write('Text Classification Frequency: Label 2')
+    # Plot using seaborn
+    fig4, ax4 = plt.subplots(figsize=(10, 6))
+    sns.barplot(data=label_counts2, y='label2', x='count', color="#4088a9")
+    
+    # Display the plot in Streamlit
+    st.pyplot(fig4)
+    st.write('Unique Combinations of Label 1 + Label 2')
+    unique_combinations = data[['label1', 'label2']].drop_duplicates()
+
+    # Show all unique combinations
+    unique_combinations.reset_index()
+    unique_combinations.sort_values(by=['label1', 'label2'])
+    st.write(unique_combinations)
     
 elif page == 'Word Cloud':
-    st.title('Word Cloud')
-    st.write('You are viewing the content of Page 2.')
-    
+    st.image('https://github.com/janduplessis883/friends-and-family-test-analysis/blob/master/images/fftest2.png?raw=true', use_column_width=True)
     st.subheader('Word Cloud')
     text = ' '.join(data['free_text'].dropna())
     wordcloud = WordCloud(background_color='white', colormap='cividis').generate(text)
@@ -148,9 +159,8 @@ elif page == 'Word Cloud':
     st.pyplot(plt)
     
 elif page == 'Show Raw Data':
-    st.title('Show Raw Data')
-    st.write('You are viewing the content of Page 2.')
-    
+    st.image('https://github.com/janduplessis883/friends-and-family-test-analysis/blob/master/images/fftest2.png?raw=true', use_column_width=True)
+    st.subheader('View Raw Data')
     # Create a slider for selecting the number of rows to display
     # Create a slider for selecting the number of rows to display
     num_rows = st.slider('Select number of rows to display:', min_value=5, max_value=100)
@@ -167,5 +177,8 @@ elif page == 'Show Raw Data':
         st.write(filtered_data.tail(num_rows))
     
 elif page == 'About':
-    st.title('About')
-    st.write('You are viewing the content of Page 2.')
+    st.image('https://github.com/janduplessis883/friends-and-family-test-analysis/blob/master/images/fftest2.png?raw=true', use_column_width=True)
+    st.subheader('About')
+    st.write("""The Friends & Family Test (FFT) is a widely-used feedback tool that aims to capture patient experiences and gauge the overall quality of healthcare services. By employing text classification and sentiment analysis techniques, we can automatically categorize patient feedback into various themes like service quality, staff behavior, or facility cleanliness. This not only streamlines the process of interpreting large volumes of free-text responses but also provides actionable insights. 
+             \nSentiment analysis further enhances this by assigning a polarity score to each response, indicating whether the sentiment is positive, negative, or neutral. This multi-layered approach allows healthcare providers to have a nuanced understanding of patient satisfaction. 
+             \nIt identifies areas for improvement, recognizes outstanding service, and ultimately, helps in making data-driven decisions to enhance patient care.""")
