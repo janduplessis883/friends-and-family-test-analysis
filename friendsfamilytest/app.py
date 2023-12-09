0c2d1be4f2f6a8d9a4054e8fcfa55c214b9df7d3
Jan du Plessis <drjanduplessis@icloud.com>
2023-12-09 06:45:54 +0000

    Automated commit via Python script - 2023-12-09 06:45

--- friendsfamilytest/app.py
+++ friendsfamilytest/app.py
@@ -90,7 +90,7 @@ if page == "Monthly Rating & Count":
                 y="rating_score",
                 data=monthly_avg_df,
                 ax=ax,
-                linewidth=5,
+                linewidth=4,
                 color="#e85d04",
             )
 
