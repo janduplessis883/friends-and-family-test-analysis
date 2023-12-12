8e6ef235b09149bb9e17a3794506619056c05cc3
Jan du Plessis <drjanduplessis@icloud.com>
2023-12-12 07:40:22 +0000

    Automated commit via Python script - 2023-12-12 07:40

--- friendsfamilytest/utils.py
+++ friendsfamilytest/utils.py
@@ -3,6 +3,7 @@ import re
 import numpy as np
 import time
 from colorama import Fore, Back, Style, init
+import functools
 
 init(autoreset=True)
 
@@ -96,3 +97,32 @@ def time_it(func):
         return result
 
     return wrapper
+
+
+def debug_info(func):
+    """Decorator that prints function execution information."""
+
+    @functools.wraps(func)
+    def wrapper_debug_info(*args, **kwargs):
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f"Calling {func.__name__}({signature})")
+
+        # Get the start time
+        start_time = time.perf_counter()
+
+        value = func(*args, **kwargs)
+
+        # Get the end time
+        end_time = time.perf_counter()
+
+        # Calculate the execution time
+        run_time = end_time - start_time
+
+        print(
+            f"Finished {func.__name__!r} in {run_time:.4f} secs with result {value!r}"
+        )
+        return value
+
+    return wrapper_debug_info
