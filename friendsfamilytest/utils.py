d3cccfcfd798ec3061328ab22f5203469200f37d
Jan du Plessis <drjanduplessis@icloud.com>
2023-12-25 17:14:53 +0000

    Automated commit via Python script - 2023-12-25 17:14

--- friendsfamilytest/utils.py
+++ friendsfamilytest/utils.py
@@ -89,7 +89,7 @@ def clean_and_replace(text):
 def time_it(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
-        print(f"{Fore.YELLOW}[F] FUCTION: {func.__name__}()")
+        print(f"{Fore.YELLOW}[🏁] FUCTION: {func.__name__}()")
         result = func(*args, **kwargs)
         print(
             f"{Fore.GREEN}{Style.DIM}[✔️] Completed: {func.__name__}() - Time taken: {time.time() - start_time:.2f} seconds"
