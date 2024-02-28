c9f1f926827d7f0e51f47bb443ee67b0af000710
Jan du Plessis <drjanduplessis@icloud.com>
2024-02-28 22:40:48 +0000

    Automated commit via Python script - 2024-02-28 22:40

--- friendsfamilytest/scheduler.py
+++ friendsfamilytest/scheduler.py
@@ -1,7 +1,17 @@
 import time
 import subprocess
+import sys
+
+wait_time = 7200  # 2 hours
 
 while True:
     subprocess.run(["python", "friendsfamilytest/data.py"])
-    time.sleep(7200)  # Wait for 7200 seconds = 2 hours
-
+    
+    # Countdown for wait_time
+    for remaining in range(wait_time, 0, -1):
+        sys.stdout.write("\r")
+        sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+        sys.stdout.flush()
+        time.sleep(1)
+    
+    sys.stdout.write("\rExecuting script...                   \n")
