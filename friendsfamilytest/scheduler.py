05c4aeea0539a9c19897a96e4c04957e2eec4b75
Jan du Plessis <drjanduplessis@icloud.com>
2024-02-29 04:49:50 +0000

    Automated commit via Python script - 2024-02-29 04:49

--- friendsfamilytest/scheduler.py
+++ friendsfamilytest/scheduler.py
@@ -1,6 +1,9 @@
 import time
 import subprocess
 import sys
+from colorama import Fore, Style, init
+
+init()  # Initializes Colorama
 
 wait_time = 7200  # 2 hours
 
@@ -10,8 +13,11 @@ while True:
     # Countdown for wait_time
     for remaining in range(wait_time, 0, -1):
         sys.stdout.write("\r")
-        sys.stdout.write("{:2d} seconds remaining.".format(remaining))
+        # Setting the color to bold red for the countdown
+        countdown_message = "{0}{1}{2} seconds remaining. Friends & Family Test".format(Fore.RED + Style.BRIGHT, remaining, Style.RESET_ALL)
+        sys.stdout.write(countdown_message)
         sys.stdout.flush()
         time.sleep(1)
     
     sys.stdout.write("\rExecuting script...                   \n")
+    sys.stdout.flush()
