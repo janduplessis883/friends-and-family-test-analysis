b98599e593d2f05f2881715bedd900daf7647f7f
Jan du Plessis <drjanduplessis@icloud.com>
2024-03-21 09:25:29 +0000

    Automated commit via Python script - 2024-03-21 09:25

--- friendsfamilytest/app.py
+++ friendsfamilytest/app.py
@@ -520,8 +520,8 @@ elif page == "PCN Dashboard":
     elif tab_selector == 'Sentiment Analysis':
         with st.container(border=False):
             labels = "Negative", "Neutral", "Positive"
-            new_data = data[data['sentiment_score'] != 1]
-            sentiment_totals = new_data.groupby('sentiment')['sentiment_score'].sum()
+
+            sentiment_totals = data.groupby('sentiment')['sentiment_score'].sum()
             colors = ['#7495a8' if sentiment == 'positive' else '#ae4f4d' if sentiment == 'negative' else '#eeeadb' for sentiment in sentiment_totals.index]
             explode = (0, 0, 0)  # 'explode' the 1st slice (Positive)
 
